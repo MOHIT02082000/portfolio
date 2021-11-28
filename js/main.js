a800493d66d2ba76bb9104614ce96b7579c572f5
MOHIT02082000 <mohitsgandhi91@gmail.com>
2021-11-28 17:37:24 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+(() =>{
+        const aboutSection = document.querySelector(".about-section"),
+        tabsContainer = document.querySelector(".about-tabs");
+
+        tabsContainer.addEventListener("click", (event) =>{
+            if(event.target.classList.contains("tab-item") && 
+              !event.target.classList.contains("active")){
+              const target = event.target.getAttribute("data-target");
+            //  deactivate existing active 'tab-item'
+            tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+            // activate new tabs
+            event.target.classList.add("active","outer-shadow");
+            aboutSection.querySelector(".tab-content.active").classList.remove("active");
+            aboutSection.querySelector(target).classList.add("active");
+            
+            }
+        })
+})();
